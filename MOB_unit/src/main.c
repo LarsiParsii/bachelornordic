f4bb415b6e5a4fdf650fa6f164f971e538c5ee91
Marius <marius.olaisen@gmail.com>
2023-04-05 16:15:35 +0200

    small changes :)

--- MOB_unit/src/main.c
+++ MOB_unit/src/main.c
@@ -5,50 +5,49 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gap.h>
 
-
 #include <nrf_modem_gnss.h>
-
-
 
 K_SEM_DEFINE(lte_connected, 0, 1);
 K_SEM_DEFINE(gnss_fix_sem, 0, 1);
 
-
-
-//LTE link controller library: 
+// LTE link controller library:
 #include <modem/lte_lc.h>
+
 
 LOG_MODULE_REGISTER(MOB_unit, LOG_LEVEL_INF);
 
 static void lte_event_handler(const struct lte_lc_evt *const evt)
 {
-    switch (evt->type) {
-        case LTE_LC_EVT_NW_REG_STATUS:
-            if (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME &&
-                evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING) { //either conected to home or roaming to continue
-                printk("Network registration status: Connected\n");
-            } else {
-                printk("Network registration status: Not connected\n");
-            }
-            break;
-        case LTE_LC_EVT_LTE_MODE_UPDATE:
-            printk("LTE mode: %s\n",
-                evt->lte_mode == LTE_LC_LTE_MODE_NONE ? "None" :
-                evt->lte_mode == LTE_LC_LTE_MODE_LTEM ? "LTE-M" :
-                "NB-IoT");
-            break;
-        case LTE_LC_EVT_MODEM_SLEEP_EXIT_PRE_WARNING:
-            printk("Modem sleep exit pre-warning: %llu seconds until modem exits sleep\n",
-                evt->modem_sleep.time);
-            break;
-        case LTE_LC_EVT_MODEM_SLEEP_EXIT:
-            printk("Modem sleep exit\n");
-            break;
-
-        case LTE_LC_EVT_MODEM_SLEEP_ENTER:
-            printk("Modem sleep enter: Duration %llu seconds\n",
+    switch (evt->type)
+    {
+    case LTE_LC_EVT_NW_REG_STATUS:
+        if (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME &&
+            evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)
+        { // either conected to home or roaming to continue
+            printk("Network registration status: Connected\n");
+        }
+        else
+        {
+            printk("Network registration status: Not connected\n");
+        }
+        break;
+    case LTE_LC_EVT_LTE_MODE_UPDATE: //maybe it may be redundant, maybe?
+        printk("LTE mode: %s\n",
+               evt->lte_mode == LTE_LC_LTE_MODE_NONE ? "None" : evt->lte_mode == LTE_LC_LTE_MODE_LTEM ? "LTE-M"
+                                                                                                      : "NB-IoT");
+        break;
+    case LTE_LC_EVT_MODEM_SLEEP_EXIT_PRE_WARNING:
+        printk("Modem sleep exit pre-warning: %llu seconds until modem exits sleep\n",
                evt->modem_sleep.time);
-            break;
+        break;
+    case LTE_LC_EVT_MODEM_SLEEP_EXIT:
+        printk("Modem sleep exit\n");
+        break;
+
+    case LTE_LC_EVT_MODEM_SLEEP_ENTER:
+        printk("Modem sleep enter: Duration %llu seconds\n",
+               evt->modem_sleep.time);
+        break;
     default:
         break;
     }
@@ -57,24 +56,21 @@ static void lte_event_handler(const struct lte_lc_evt *const evt)
 static void modem_configure(void)
 {
     int err = lte_lc_init_and_connect_async(lte_event_handler);
-    if (err) {
+    if (err)
+    {
         printk("Modem could not be configured, error: %d\n", err);
         return;
     }
 }
 
-int main(void) //can also use void main(void)?
+int main(void) // can also use void main(void)?
 {
     modem_configure();
 
-    
-
-    while(1){
-
-
+    while (1)
+    {
     }
     lte_lc_power_off();
     LOG_ERR("Error. Shutting down.");
 
 }
-
